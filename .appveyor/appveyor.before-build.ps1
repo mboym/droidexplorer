a70fa3fd03def1469ac81378c5c9bc7dc98681d6
mboym <markovskim89@yahoo.com>
2017-04-02 03:22:47 +0200

    Update appveyor.before-build.ps1

--- .appveyor/appveyor.before-build.ps1
+++ .appveyor/appveyor.before-build.ps1
@@ -2,7 +2,7 @@ Import-Module "$env:APPVEYOR_BUILD_FOLDER\.appveyor\modules\Import-PfxCertificat
 Import-Module "$env:APPVEYOR_BUILD_FOLDER\.appveyor\modules\Set-BuildVersion.psm1";
 
 
-Import-PfxCertificate -pfx "$env:APPVEYOR_BUILD_FOLDER\Shared\droidexplorer.pfx" -password ((Get-Item Env:$DE_PFX_KEY).Value) -containerName ((Get-Item Env:$VS_PFX_KEY).Value);
+Import-PfxCertificate -pfx "$env:APPVEYOR_BUILD_FOLDER\Shared\droidexplorer.pfx" -password ((Get-Item Env:\DE_PFX_KEY).Value) -containerName ((Get-Item Env:\VS_PFX_KEY).Value);
 
 $env:CI_BUILD_DATE = ((Get-Date).ToUniversalTime().ToString("MM-dd-yyyy"));
 $env:CI_BUILD_TIME = ((Get-Date).ToUniversalTime().ToString("hh:mm:ss"));
